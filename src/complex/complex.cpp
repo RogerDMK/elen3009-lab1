8b78bf7d5a468d6f89358d8ce535059d0425feb0
Thendo Rabali <2091520@students.wits.ac.za>
2021-08-15 13:39:18 +0200

    Exercise 3.2

--- src/complex/complex.cpp
+++ src/complex/complex.cpp
@@ -7,11 +7,11 @@
 // Exercise 3.1 Any line of text/code between two forward slashes and the end of line will be ignored by the compiler,therefore will not execute
 
 using namespace std; // cout, endl, complex are all part of this namespace
-
+using complx = complex<float>;
 int main()
 {
-	auto num1 = complex<float>{2.0, 2.0};  // use auto for type deduction
-	auto num2 = complex<float>{4.0, -2.0}; // use uniform initialisation syntax (curly braces)
+	auto num1 = complx{2.0, 2.0};  // use auto for type deduction
+	auto num2 = complx{4.0, -2.0}; // use uniform initialisation syntax (curly braces)
 
 	auto answer = num1 * num2; // type deduced for 'answer' is: complex<float>
 
